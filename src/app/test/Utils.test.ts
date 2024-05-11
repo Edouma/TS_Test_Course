c45e691316b7d0a6a66bb95c8bbfb703c94736bf
Edwin <edoumacodu@gmail.com>
2024-05-11 12:37:14 +0300

    adde testing to check for valid string

--- src/app/test/Utils.test.ts
+++ src/app/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../Utils"
+import { getStringInfo, toUpperCase } from "../Utils"
 
 describe('Utils test ', ()=>{
     it('should return uppercase', () => {
@@ -11,5 +11,16 @@ describe('Utils test ', ()=>{
         
         //assert
         expect(actual).toBe(expected);
+    })
+
+    it('should return info for valid string', () =>{
+        const actual = getStringInfo('My-String');
+
+        expect(actual.lowerCase).toBe('my-string');
+        expect(actual.extraInfo).toEqual({});
+
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
+
     })
 })
